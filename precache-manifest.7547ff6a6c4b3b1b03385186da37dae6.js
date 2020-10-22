453385ff156d49be3f17c6e5b54e0accdc4336e7
KalpTalwadia <talwadiakalp@gmail.com>
2020-10-22 13:08:39 +0530

    Updates

---
+++ precache-manifest.7547ff6a6c4b3b1b03385186da37dae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802593bc296c4483ac50890bd4f7f29b",
+    "url": "/Hearing-Test.git/index.html"
+  },
+  {
+    "revision": "351fdc1e91ff258a86e3",
+    "url": "/Hearing-Test.git/static/css/main.a1d97fa4.chunk.css"
+  },
+  {
+    "revision": "f5129ab5541a3ec9b2ad",
+    "url": "/Hearing-Test.git/static/js/2.d4839073.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/Hearing-Test.git/static/js/2.d4839073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "351fdc1e91ff258a86e3",
+    "url": "/Hearing-Test.git/static/js/main.d71a5522.chunk.js"
+  },
+  {
+    "revision": "80953fd113297937b706",
+    "url": "/Hearing-Test.git/static/js/runtime-main.990c3020.js"
+  }
+]);
